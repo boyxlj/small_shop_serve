7b1d761e38edfad47203eb958aa3dac7ff481756
Miraitowa <x709500@163.com>
2022-10-03 15:43:36 +0800

    微商城服务端

---
+++ .history/config/sql_20220715084826.js
@@ -0,0 +1,9 @@
+const mysqlConfig = {
+  host:"60.205.226.237",
+  database:"wei_shop",
+  user:"wei_shop",
+  password:"wasd123456",
+  port:"3306"
+}
+
+module.exports = mysqlConfig
---
+++ .history/config/sql_20221003154213.js
@@ -0,0 +1,9 @@
+const mysqlConfig = {
+  host:"localhost",
+  database:"wei_shop",
+  user:"wei_shop",
+  password:"",
+  port:"3306"
+}
+
+module.exports = mysqlConfig
